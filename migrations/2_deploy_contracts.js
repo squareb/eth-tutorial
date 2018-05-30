bc4ad8a884061d1c95831e37b6f441bd6b42ecae
Bas <basbolmer@gmail.com>
2018-05-30 11:16:31 +0200

    Added a RUN.txt for cmd commands and the first draft for an 'Handshake'-protocol

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
-const Wrestling = artifacts.require("./Wrestling.sol")
+//const Wrestling = artifacts.require("./Wrestling.sol")
+const Handshake = artifacts.require("./Handshake.sol")
 
 module.exports = function(deployer) {
-	deployer.deploy(Wrestling);
+	//deployer.deploy(Wrestling);
+	deployer.deploy(Handshake);
 };
